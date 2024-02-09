77741c88d59e2cf8b62edac0c0662af80616677d
kewalaka <kewalaka@gmail.com>
2024-02-09 16:18:37 +1300

    feat: add private endpoints & bump provider versions

--- examples/private-endpoint/main.tf
+++ examples/private-endpoint/main.tf
@@ -17,8 +17,6 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-
-## Section to provide a random Azure region for the resource group
 # This allows us to randomize the region for the resource group.
 module "regions" {
   source  = "Azure/regions/azurerm"
@@ -30,19 +28,37 @@ resource "random_integer" "region_index" {
   min = 0
   max = length(module.regions.regions) - 1
 }
-## End of section to provide a random Azure region for the resource group
 
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
   source  = "Azure/naming/azurerm"
-  version = ">= 0.4.0"
+  version = "0.4.0"
 }
-
 
 # This is required for resource modules
 resource "azurerm_resource_group" "this" {
   name     = module.naming.resource_group.name_unique
   location = module.regions.regions[random_integer.region_index.result].name
+}
+
+# A vnet & subnet is required for the private endpoint.
+resource "azurerm_virtual_network" "this" {
+  name                = module.naming.virtual_network.name_unique
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["192.168.0.0/24"]
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = module.naming.subnet.name_unique
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["192.168.0.0/24"]
+}
+
+resource "azurerm_private_dns_zone" "this" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "random_password" "admin_password" {
@@ -51,10 +67,6 @@ resource "random_password" "admin_password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-# This is the module call
-# Do not specify location here due to the randomization above.
-# Leaving location as `null` will cause the module to use the resource group location
-# with a data source.
 module "mysql_server" {
   source = "../../"
   # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
@@ -65,4 +77,23 @@ module "mysql_server" {
   administrator_login    = "mysqladmin"
   administrator_password = random_password.admin_password.result
   sku_name               = "GP_Standard_D2ds_v4"
+
+  private_endpoints = {
+    primary = {
+      private_dns_zone_resource_ids = [azurerm_private_dns_zone.this.id]
+      subnet_resource_id            = azurerm_subnet.this.id
+    }
+  }
+}
+
+check "dns" {
+  data "azurerm_private_dns_a_record" "assertion" {
+    name                = module.naming.mysql_server.name_unique
+    zone_name           = "privatelink.mysql.database.azure.com"
+    resource_group_name = azurerm_resource_group.this.name
+  }
+  assert {
+    condition     = one(data.azurerm_private_dns_a_record.assertion.records) == one(module.mysql_server.private_endpoints["primary"].private_service_connection).private_ip_address
+    error_message = "The private DNS A record for the private endpoint is not correct."
+  }
 }
--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71.0"
+      version = ">= 3.91.0, < 4.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0"
+      version = ">= 3.6.0, < 4.0.0"
     }
   }
 }
