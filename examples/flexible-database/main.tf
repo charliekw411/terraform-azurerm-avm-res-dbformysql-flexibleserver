af49861ad8f1b2b1401ffea82f64a1edec3db0c2
kewalaka <kewalaka@gmail.com>
2024-02-09 20:03:03 +1300

    chore: bump provider versions

--- examples/flexible-database/main.tf
+++ examples/flexible-database/main.tf
@@ -3,17 +3,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0, < 4.0.0"
+      version = ">= 3.91.0, < 4.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
+      version = ">= 3.6.0, < 4.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
 # This ensures we have unique CAF compliant names for our resources.
